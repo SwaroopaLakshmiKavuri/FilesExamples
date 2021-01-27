e6e54b2d6ed1cca3e3e425378419c1dcba23f02c
SwaroopaLakshmi Kavuri <swaroopalakshmi.kavuri@zensar.com>
2021-01-27 08:11:29 +0530

    Add project files.

---
+++ FilesExamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FilesExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream(@"Sample1.txt", FileMode.Create, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                Console.WriteLine("Enter the text");
+                Console.WriteLine("Enter EOF to end the file");
+                string s = null;
+                while (s != "EOF")
+                {
+                    s = Console.ReadLine();
+                    if (s != "EOF")
+                        sw.WriteLine(s);
+                }
+            }
+            using (FileStream fs1 = new FileStream(@"Sample1.txt", FileMode.Open))
+            using (StreamReader sr = new StreamReader(fs1))
+            {
+                Console.WriteLine($"Reading contents to Sample.txt :{sr.ReadToEnd()}");
+            }
+        }
+    }
+}
